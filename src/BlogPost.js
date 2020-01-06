be44ee353209571f05f9036f50a531690227459e
Tania Manco <belalov.tania16@gmail.com>
2020-01-06 19:28:50 +0000

    Automatically backed up by Learn

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -22,7 +22,6 @@ export default class BlogPost extends Component {
         <Comment commentText={comments[0]} />
         <Comment commentText={comments[1]} />
         <Comment commentText={comments[2]} />
-       
       </div>
     );
   }
